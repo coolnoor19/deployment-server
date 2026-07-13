d30642143c6599c4593d208ce3767ded6cc52c04
Noor Alam <noor.qualysec@gmail.com>
2026-07-13 16:13:19 +0530

    add lines

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello ChaiCode Team😊😊' })
+  return res.json({ message: 'Hello Qualysec Team😊😊' })
 })
 
 app.listen(PORT, ()=>{
